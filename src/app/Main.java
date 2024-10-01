e6f8422b3cbde81f0ac4a7e080e11c46aa405c31
parallelepiped1337 <andrejbuiukli@gmail.com>
2024-10-01 18:03:19 +0300

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,31 +1,28 @@
-package app;
-
 public class Main {
 
     public static void main(String[] args) {
 
         String product1 = "smartphone";
-        double totalSales1 = 12153.41;
+        double pricePerUnit1 = 243.0682;
+        int quantitySoldPerDay1 = 10;
         int days1 = 5;
 
+        double totalSales1 = pricePerUnit1 * quantitySoldPerDay1 * days1;
+        double dailySales1 = totalSales1 / days1;
+
+        System.out.printf("Product No 1: %s,\ntotal sales for %d days is EUR %.2f,\nsales by day is EUR %.2f.\n",
+                product1, days1, totalSales1, dailySales1);
+
         String product2 = "laptop";
-        double totalSales2 = 10486.85;
+        double pricePerUnit2 = 149.812;
+        int quantitySoldPerDay2 = 10;
         int days2 = 7;
 
-        double salesByDay1 = totalSales1 / days1;
-        double salesByDay2 = totalSales2 / days2;
+        double totalSales2 = pricePerUnit2 * quantitySoldPerDay2 * days2;
+        double dailySales2 = totalSales2 / days2;
 
-        System.out.printf("Product No 1: %s,%n", product1);
-
-        System.out.printf("total sales for %d days is EUR %.2f,%n", days1, totalSales1);
-
-        System.out.printf("sales by day is EUR %.2f.%n", salesByDay1);
-
-        System.out.printf("Product No 2: %s,%n", product2);
-
-        System.out.printf("total sales for %d days is EUR %.2f,%n", days2, totalSales2);
-
-        System.out.printf("sales by day is EUR %.2f.%n", salesByDay2);
+        System.out.printf("Product No 2: %s,\ntotal sales for %d days is EUR %.2f,\nsales by day is EUR %.2f.\n",
+                product2, days2, totalSales2, dailySales2);
 
     }
 
